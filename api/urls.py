aabc131ebe416214dc8971c8a923aecfc0c77330
Farid Erfan <far.er27@gmail.com>
2026-05-10 15:31:32 +0330

    Add time endpoint

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import hello_view
+from .views import hello_view, time_view
 
 urlpatterns = [
     path('hello/', hello_view, name='hello'),
+    path('time/', time_view, name='time'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,7 +1,13 @@
 from django.http import JsonResponse
+from django.utils import timezone
 
 # Create your views here.
 
 
 def hello_view(request):
     return JsonResponse({'message': 'hello'})
+
+
+def time_view(request):
+    now = timezone.now()
+    return JsonResponse({'time': str(now)})
