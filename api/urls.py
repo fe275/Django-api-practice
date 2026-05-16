89dc1cc5d0f0455f2d86ee77ddf9167041cb9c27
Farid Erfan <far.er27@gmail.com>
2026-05-16 21:02:04 +0330

    Add DRF and Serializers.

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import hello_view, time_view
+from .views import hello_view, time_view, product_list
 
 urlpatterns = [
     path('hello/', hello_view, name='hello'),
     path('time/', time_view, name='time'),
+    path('products/', product_list),
 ]
--- api/views.py
+++ api/views.py
@@ -1,5 +1,9 @@
 from django.http import JsonResponse
 from django.utils import timezone
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Product
+from .serializers import ProductSerializer
 
 # Create your views here.
 
@@ -11,3 +15,10 @@ def hello_view(request):
 def time_view(request):
     now = timezone.now()
     return JsonResponse({'time': str(now)})
+
+
+@api_view(['GET'])
+def product_list(request):
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
