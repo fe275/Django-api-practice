3d668cd77e493f79ef1b98d2ff04b484217b0109
Farid Erfan <far.er27@gmail.com>
2026-05-17 18:58:24 +0330

    add product creation with validation

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import hello_view, time_view, product_list
+from .views import hello_view, time_view, product_list, add_product
 
 urlpatterns = [
     path('hello/', hello_view, name='hello'),
     path('time/', time_view, name='time'),
     path('products/', product_list, name='products'),
+    path('add/', add_product, name='add product')
 ]
--- api/views.py
+++ api/views.py
@@ -22,3 +22,12 @@ def product_list(request):
     products = Product.objects.all()
     serializer = ProductSerializer(products, many=True)
     return Response(serializer.data)
+
+
+@api_view(['POST'])
+def add_product(request):
+    serializer = ProductSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=201)
+    return Response(serializer.errors, status=400)
